801ee30913f144a464ddf44a80f92d4443f20130
juhairulislam <juhairulislam2018@gmail.com>
2026-04-30 20:03:02 +0600

    add hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import SunCartHero from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    
+   <div className="md:w-[85%] mx-auto">
+    <SunCartHero></SunCartHero>
    </div>
   );
 }
