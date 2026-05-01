8408a6364256ea933aa61ba39dd4df2088faa202
juhairulislam <juhairulislam2018@gmail.com>
2026-05-01 16:29:20 +0600

    upgrate product card design

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { FaStarHalfAlt } from 'react-icons/fa';
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 
 const products = async () => {
@@ -23,14 +23,14 @@ const PopularProducts = async () => {
     return (
         <div>
             <div className='p-10 my-6'>
-                <h1 className='text-center text-4xl font-bold text-gray-600'>All Products From SunCart</h1>
+                <h1 className='text-center text-2xl md:text-4xl font-bold text-gray-600'>All Products From SunCart</h1>
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6'>
 
                     {
                         AllProducts.map(p => <div key={p.id}
                         >
-                            <div className='rounded-xl shadow p-6'>
+                            <div className='rounded-xl shadow p-6 hover:shadow-xl transition duration-300'>
 
                                 <Image
                                     alt={p.name}
@@ -42,13 +42,21 @@ const PopularProducts = async () => {
 
                                 </Image>
 
-                                <h1 className='my-2 font-bold text-xl'>{p.name}</h1>
-                                <p className='flex gap-2 items-center font-semibold text-xl'><span className='text-yellow-300'><FaStarHalfAlt />
-                                </span> {p.rating}</p>
+                                <h1 className='my-6 font-bold text-xl'>{p.name}</h1>
+                                 <div className="flex items-center gap-1 text-yellow-400 text-sm">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                  <span className="text-gray-600 ml-1 text-md">
+                    ({p.rating})
+                  </span>
+                </div>
 
-                                <div className='flex items-center justify-between mt-6'>
-                                    <p className='font-bold text-2xl'>$ {p.price}</p>
-                                    <Link href={`/products/${p.id}`} className='btn text-white bg-orange-400'>View Details</Link>
+                                <div className='flex items-center justify-between mt-4'>
+                                    <p className='text-2xl font-bold text-orange-500'>${p.price}</p>
+                                    <Link href={`/products/${p.id}`} className='btn rounded-xl bg-linear-to-r from-orange-400 to-pink-500 text-white hover:opacity-90 transition'>View Details</Link>
 
                                 </div>
 
