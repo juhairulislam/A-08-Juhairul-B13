bf50325b4e4cfc5f6727941e1fad2013267132a4
juhairulislam <juhairulislam2018@gmail.com>
2026-05-01 11:06:43 +0600

    change text in products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -23,7 +23,7 @@ const PopularProducts = async () => {
     return (
         <div>
             <div className='p-10 my-6'>
-                <h1 className='text-center text-4xl font-bold text-gray-600'>Popular Products</h1>
+                <h1 className='text-center text-4xl font-bold text-gray-600'>All Products From SunCart</h1>
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6'>
 
