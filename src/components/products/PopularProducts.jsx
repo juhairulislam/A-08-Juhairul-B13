0a1ac00d32dd468438d4e63439b25b3a28b45c4d
juhairulislam <juhairulislam2018@gmail.com>
2026-05-01 11:00:12 +0600

    update json photo url and show products in products route

--- src/components/products/PopularProducts.jsx
+++ src/components/products/PopularProducts.jsx
@@ -16,13 +16,13 @@ const PopularProducts = async () => {
 
     const productsData = await products()
 
-    const AllProducts = productsData.slice(3,6 );
+    const AllProducts = productsData.slice(0,3 );
 
     // console.log(AllProducts)
 
     return (
         <div>
-            <div className='p-10 my-6'>
+            <div className='p-4 md:p-10 my-6'>
                 <h1 className='text-center text-4xl font-bold text-gray-600'>Popular Products</h1>
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6'>
