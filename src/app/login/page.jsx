6d4ded8be9c947cdde94755ae9ab9b24a3380a89
juhairulislam <juhairulislam2018@gmail.com>
2026-05-01 20:10:49 +0600

    signup setup completed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,50 +3,57 @@ import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 
 const LoginPage = () => {
-    return (
-        <div>
-            <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-orange-500 to-orange-300">
-      <div className="bg-white p-8 rounded-2xl shadow-xl w-96">
-        <h2 className="text-2xl font-bold text-center mb-6 text-orange-500">
-          SunCart Login ☀️
-        </h2>
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-orange-500 to-orange-300">
+        <div className="bg-white p-8 rounded-2xl shadow-xl w-96">
+          <h2 className="text-2xl font-bold text-center mb-6 text-orange-500">
+            SunCart Login ☀️
+          </h2>
 
-        <form  className="space-y-2">
+          <form className="space-y-3">
 
-            <p>Email</p>
+            {/* Email */}
+            <div>
+              <label className="text-sm font-medium">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
 
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full p-3 border rounded-lg focus:outline-orange-400"
-          />
+            {/* Password */}
+            <div>
+              <label className="text-sm font-medium">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
 
-            <p>Password</p>
+            <button className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+              Login
+            </button>
+          </form>
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 border rounded-lg focus:outline-orange-400"
-          />
+          <div className="mt-4 text-center text-gray-500">OR</div>
 
-          <button className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
-            Login
+          <button
+            className="w-full mt-4 border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100"
+          >
+            <FcGoogle />
+            Continue with Google
           </button>
-        </form>
-
-        <div className="mt-4 text-center text-gray-500">OR</div>
-
-        <button
-          className="w-full mt-4 border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100"
-        >
-               <FcGoogle />
-      
-          Continue with Google
-        </button>
+        </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default LoginPage;
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,58 +1,106 @@
+'use client'
 import Image from 'next/image';
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import { authClient } from "@/lib/auth-client"; //import the auth client
+
 
 const SignUpPage = () => {
-    return (
-        <div>
- <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-orange-500 to-orange-300">
-      <div className="bg-white p-6 rounded-2xl shadow-xl w-96">
-        <h2 className="text-2xl font-bold text-center mb-4 text-orange-500">
-          Create Account 🌞
-        </h2>
 
-        <form  className="space-y-2">
+  const handleSubmit =async (e) =>{
 
-            <p>Email</p>
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full p-3 border rounded-lg focus:outline-orange-400"
-          />
+  e.preventDefault();
+  const name = e.target.name.value ;
+   const email = e.target.email.value;
+  const password = e.target.password.value;
+  const image = e.target.photo.value ;
 
-            <p>Password</p>
+  // console.log({name , email , password , image})
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 border rounded-lg focus:outline-orange-400"
-          />
+  const { data, error } = await authClient.signUp.email({
 
-                      <p>Photo URL</p>
+    name,
+    email,
+    password,
+    image
 
-          <input
-            type="text"
-            placeholder="Photo URL"
-            className="w-full p-3 border rounded-lg focus:outline-orange-400"
-          />
+  })
 
-          <button className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
-            Register
+  console.log({data , error})
+
+  }
+
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-orange-500 to-orange-300">
+        <div className="bg-white p-6 rounded-2xl shadow-xl w-96">
+          <h2 className="text-2xl font-bold text-center mb-4 text-orange-500">
+            Create Account 🌞
+          </h2>
+
+          <form className="space-y-3" onSubmit={handleSubmit}>
+
+                <div>
+              <label className="text-sm font-medium">Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your Name"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm font-medium">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm font-medium">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm font-medium">Photo URL</label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Enter photo URL"
+                required
+                className="w-full p-3 border rounded-lg focus:outline-orange-400"
+              />
+            </div>
+
+            <button className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+              Register
+            </button>
+          </form>
+
+          <div className="mt-4 text-center text-gray-500">OR</div>
+
+          <button
+            className="w-full mt-4 border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100"
+          >
+            <FcGoogle />
+            Sign up with Google
           </button>
-        </form>
-
-        <div className="mt-4 text-center text-gray-500">OR</div>
-
-        <button
-          className="w-full mt-4 border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100"
-        >
-         <FcGoogle />
-
-          Sign up with Google
-        </button>
+        </div>
       </div>
-    </div>        </div>
-    );
+    </div>
+  );
 };
 
 export default SignUpPage;
