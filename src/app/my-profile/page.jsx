524921518f0ce1f213b20ba09308d79b9d9e354c
juhairulislam <juhairulislam2018@gmail.com>
2026-05-02 18:20:14 +0600

    unnececery text removed from update profile page

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -13,17 +13,16 @@ const MyProfilePage = () => {
 
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-orange-500 to-orange-300 px-4">
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-orange-500 to-orange-300 px-4">
 
             <div className="bg-white/90 backdrop-blur-md shadow-2xl rounded-3xl p-6 sm:p-8 w-full max-w-md text-center">
 
-                {/* Avatar */}
                 <div className="flex justify-center mb-4">
                     <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-full ring-4 ring-orange-500 ring-offset-2 overflow-hidden">
                         <Image
                             referrerPolicy="no-referrer"
-                            alt={user?.name || "User"}
-                            src={user?.image || "/default-avatar.png"}
+                            alt={user?.name}
+                            src={user?.image }
                             width={96}
                             height={96}
                             className="object-cover w-full h-full"
@@ -31,17 +30,14 @@ const MyProfilePage = () => {
                     </div>
                 </div>
 
-                {/* Name */}
                 <h2 className="text-lg sm:text-xl font-bold text-gray-800">
-                    {user?.name || "Guest User"}
+                    {user?.name}
                 </h2>
 
-                {/* Email */}
                 <p className="text-gray-500 text-sm sm:text-base mb-6">
-                    {user?.email || "No Email"}
+                    {user?.email}
                 </p>
 
-                {/* Button */}
                 <button onClick={() => setOpenModal(true)} className="w-full bg-orange-500 hover:bg-orange-600 transition-all duration-300 text-white py-2.5 sm:py-3 rounded-xl font-semibold text-sm sm:text-base">
                     Edit Profile
                 </button>
